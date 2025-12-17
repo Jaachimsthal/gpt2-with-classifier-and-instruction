f6f6bfd16ac50424c84d16362f705de5ffa55873
jiangshuaibing <jiangshuaibing@hourenkeji.com>
2025-12-17 10:28:45 +0800

    update gpt params

--- GPTModel.py
+++ GPTModel.py
@@ -385,14 +385,28 @@ def token_ids_to_text(token_ids: torch.Tensor, tokenizer: Encoding) -> str:
 from gpt_download import download_and_load_gpt2
 settings, params = download_and_load_gpt2(model_size="124M", models_dir="gpt2")
 
+model_configs = {
+    "gpt2-small(124M)":  {"emb_dim": 768,  "n_layers": 12, "n_heads": 12},
+    "gpt2-medium(355M)": {"emb_dim": 1024, "n_layers": 24, "n_heads": 16},
+    "gpt2-large(774M)":  {"emb_dim": 1280, "n_layers": 36, "n_heads": 20},
+    "gpt2-xl(1558M)":    {"emb_dim": 1600, "n_layers": 48, "n_heads": 25},
+}
+
+# 更新之前使用的GPT_CONFIG_124M
+model_name = "gpt2-small(124M)"
+NEW_CONFIG = GPT_CONFIG_124M.copy()
+NEW_CONFIG.update(model_configs[model_name])
+NEW_CONFIG.update("context_length", 1024) # 原始GPT-2模型使用1024个词元长度进行训练
+NEW_CONFIG.update("qkv_bias", True)
+
 tokenizer = tiktoken.get_encoding("gpt2")
 
 torch.manual_seed(123)
 token_ids = generate(
-    model=GPTModel2(cfg=GPT_CONFIG_124M),
+    model=GPTModel2(cfg=NEW_CONFIG),
     idx=text_to_token_ids("Every effort moves you", tokenizer=tokenizer),
     max_new_tokens=15,
-    context_size=GPT_CONFIG_124M['context_length'],
+    context_size=NEW_CONFIG['context_length'],
     top_k=25,
     temperature=1.4
 )
